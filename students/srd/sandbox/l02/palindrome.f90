161b3ae1318ce84fd866882ff90412c0201408ce
Daniel <alu0101430263@ull.edu.es>
2025-09-24 15:04:34 +0100

    Fixed l02

--- students/srd/sandbox/l02/palindrome.f90
+++ students/srd/sandbox/l02/palindrome.f90
@@ -1,28 +1,27 @@
 program palindrome
     implicit none
     integer :: n
-    character(len=50) :: numStr, revStr
+    character(len=50) :: numStr, revStr ! Can also be done with allocate to a dynamical lenght
     integer :: i, lenN
 
-    ! Pedir número
-    print *, "Introduce un número entero:"
+    print *, "Introduce a positive integer to check if it's a palindrome:"
     read *, n
 
     ! Convertir a cadena
-    write(numStr, '(I0)') n   ! I0: convierte entero sin espacios
-    lenN = len_trim(numStr)   ! longitud real
+    write(numStr, '(I0)') n   ! I0: convert without spaces
+    lenN = len_trim(numStr)   ! number of digits
 
-    ! Construir cadena invertida
+    ! String with the number read backwards
     revStr = ""
     do i = 1, lenN
         revStr(i:i) = numStr(lenN-i+1:lenN-i+1)
     end do
 
-    ! Comprobar
+    ! Compare the two numbers ( we trim to delete the blank spaces )
     if (trim(numStr(1:lenN)) == trim(revStr(1:lenN))) then
-        print *, n, "es capicúa"
+        print *, n, "Is a palindrome!"
     else
-        print *, n, "NO es capicúa"
+        print *, n, "Is not a palindrome"
     end if
 
 end program palindrome
