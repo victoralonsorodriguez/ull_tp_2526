7de4b036b74dcf31b498361602d1eb559ef1c21f
Cris-bq <xtinabcs@gmail.com>
2025-10-08 13:32:26 +0100

    Add particle module

---
+++ students/cbq/course_exercise_1/particle.f90
@@ -0,0 +1,12 @@
+module particle
+use geometry, only: point3d, vector3d
+    implicit none
+    type :: particle3d
+        type(point3d) :: p  ! position
+        type(vector3d) :: v   ! velocity
+        real(kind=8) :: m   ! mass
+    end type particle3d
+
+contains
+    
+end module particle
