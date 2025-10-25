26a5d3c5ec15179e250c96470abcde4e88a2bf35
josemiguelmp-ugr <josemiguelmp@ugr.es>
2025-10-25 12:34:16 +0200

    Removed local dp definitions since dp is already defined in the geometry module

--- students/mpj/course_exercise_1/particle.f90
+++ students/mpj/course_exercise_1/particle.f90
@@ -2,8 +2,6 @@ module particle
     use geometry
     implicit none
 
-    integer, parameter :: dp = SELECTED_REAL_KIND(p=15, r=307) ! Defining our desired precision (64 bits)
-
     type particle3d
         type(point3d)  :: p
         type(vector3d) :: v
